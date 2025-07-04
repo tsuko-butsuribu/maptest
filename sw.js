861c193e004eea9f7afef0a4165167ff8c11b2b8
tsuko-butsuribu <tsu25538d@mie-c.ed.jp>
2025-07-04 18:03:48 +0900

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,6 +1,12 @@
 // PWA対応：Service Worker
+
 var CACHE_NAME = 'task-management';
 var urlsToCache = [
+    './', // index.html のルート
+    './index.html',
+    './index.css',
+    './manifest.json',
+    './img/icon.png' // アイコン画像もキャッシュ対象にする
 ];
 
 self.addEventListener('install', function(event) {
@@ -8,6 +14,7 @@ self.addEventListener('install', function(event) {
         caches
             .open(CACHE_NAME)
             .then(function(cache) {
+                console.log('Opened cache'); // キャッシュが開かれたことを確認
                 return cache.addAll(urlsToCache);
             })
     );
@@ -18,6 +25,7 @@ self.addEventListener('fetch', function(event) {
         caches
             .match(event.request)
             .then(function(response) {
+                // キャッシュにレスポンスがあればそれを返し、なければネットワークからフェッチ
                 return response || fetch(event.request);
             })
     );
