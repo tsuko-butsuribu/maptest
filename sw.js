f466faedf9494e2892e235a287c325fbbf61a7d2
tsuko-butsuribu <tsu25538d@mie-c.ed.jp>
2025-07-04 17:06:56 +0900

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,25 @@
+// PWA対応：Service Worker
+
+var CACHE_NAME = 'task-management';
+var urlsToCache = [
+];
+
+self.addEventListener('install', function(event) {
+    event.waitUntil(
+        caches
+            .open(CACHE_NAME)
+            .then(function(cache) {
+                return cache.addAll(urlsToCache);
+            })
+    );
+});
+
+self.addEventListener('fetch', function(event) {
+    event.respondWith(
+        caches
+            .match(event.request)
+            .then(function(response) {
+                return response || fetch(event.request);
+            })
+    );
+});
